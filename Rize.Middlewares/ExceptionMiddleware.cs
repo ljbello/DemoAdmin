4854ff12c20f1dbfd24410647f7feacec9fd4a19
Luis Bello <lbello@complyright.com>
2024-10-18 13:31:40 -0400

    ok

---
+++ Rize.Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,79 @@
+﻿
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.FileProviders;
+
+namespace Rize.Middlewares
+
+{
+    public static class ExceptionMiddlewareExtension
+    {
+        public static void UseExceptionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+
+    public class ExceptionMiddleware
+    {
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly RequestDelegate _next;
+        private readonly IHostEnvironment _env;
+
+        public ExceptionMiddleware(ILogger<ExceptionMiddleware> logger, RequestDelegate next, IHostEnvironment env)
+        {
+            _logger = logger;
+            _next = next;
+            _env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception exception)
+            {
+                await HandleExceptionAsync(httpContext, exception);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            _logger.LogError(exception, "An error occurred.");
+
+            // Check if the request is an AJAX request
+            
+            if (context.Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+            {
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+                var errorResponse = new
+                {
+                    message = "An error occurred while processing your request.",
+                    details = _env.IsDevelopment() ? exception.Message : "Please contact support."
+                };
+
+                await context.Response.WriteAsync(JsonSerializer.Serialize(errorResponse));
+            }
+            else if (exception.Message.ToLower().Contains("notfound"))
+            {
+                context.Response.Redirect("/Home/NotFound");
+            }
+            else
+            {
+                // Redirect to a custom error page
+                context.Response.Redirect("/Home/Error");
+            }
+        }
+    }
+}
+
+
