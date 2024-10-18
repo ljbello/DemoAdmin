7899eb03428061b2e39290c00a3875a4bfee5d7b
Luis Bello <lbello@complyright.com>
2024-10-18 12:52:03 -0400

    componentes

---
+++ Rize.Admin/Components/HeaderViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rize.Admin.Services.Interface;
+
+namespace RIZE.AdminWeb.Components
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+
+        private readonly IAccountService _accountService;
+
+        public HeaderViewComponent(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+           // await _accountService.GetLoggedUserInformation();
+            return View();
+        }
+    }
+}
---
+++ Rize.Admin/Components/SidebarMenuViewComponent .cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rize.Admin.Services.Interface;
+
+namespace RIZE.AdminWeb.Components
+{
+    public class SidebarMenuViewComponent : ViewComponent
+    {
+
+        private readonly IAccountService _accountService;
+
+        public SidebarMenuViewComponent(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            // Add your logic to fetch the sidebar items, such as from user privileges or database
+            var menuItems = await _accountService.GetMenusByUserId(2121065); // Example method
+            return View(menuItems);
+        }
+    }
+}
